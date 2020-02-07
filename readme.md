67e86c64fe712ae422f10a96c5bbd4822c578221
David Edson <eddo888@tpg.com.au>
2020-02-07 18:22:41 +1100

    libterm

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 logging decorator
+
+python setup.py install  --install-purelib "/private/var/mobile/Containers/Data/Application/1C24CB13-4B83-4191-A5DF-7230A902AA60/Documents/site-packages3" 
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='Baubles',
+  version='1.0',
+  description='logging decorator',
+  url='git@github.com:eddo888/baubles.git',
+  author='David Edson',
+  author_email='eddo888@tpg.com.au',
+  license='MIT',
+  packages=[
+  ],
+  install_requires=[
+  ],
+  zip_safe=False
+)
