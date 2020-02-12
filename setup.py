807468537e8c4a94932beaa1871d2e716aa07292
David Edson <eddo888@tpg.com.au>
2020-02-12 12:26:15 +1100

    ok

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+import setuptools
 
-setup(name='Baubles',
-  version='1.0',
-  description='logging decorator',
-  url='git@github.com:eddo888/baubles.git',
-  author='David Edson',
-  author_email='eddo888@tpg.com.au',
-  license='MIT',
-  packages=[
-      'Baubles'
-  ],
-  install_requires=[
-  ],
-  #zip_safe=False
+setuptools.setup(
+	name='Baubles',
+	version='1.0',
+	description='logging decorator',
+	url='git@github.com:eddo888/baubles.git',
+	author='David Edson',
+	author_email='eddo888@tpg.com.au',
+	license='MIT',
+	packages=setuptools.find_packages(),
+	#install_requires=[],
+	#python_requires='>=3.6',
 )
