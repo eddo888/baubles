bf9ac039fa9bc0a2a3226fb631c2c23aa1d495d2
David Edson <dave@stibosystems.com.au>
2020-02-13 23:06:37 +1100

    readme

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
+from os import path
 from distutils.core import setup
 
+pwd = path.abspath(path.dirname(__file__))
+with open(path.join(pwd, 'README.md')) as input:
+    long_description = input.read()
+	
 setup(
 	name='Baubles',
-	version='1.2',
+	version='1.3',
 	license='MIT',
 	description='logging decorator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 	url='https://github.com/eddo888/baubles',
-	download_url='https://github.com/eddo888/baubles/archive/1.2.tar.gz',
+	download_url='https://github.com/eddo888/baubles/archive/1.3.tar.gz',
 	author='David Edson',
 	author_email='eddo888@tpg.com.au',
 	packages=[
